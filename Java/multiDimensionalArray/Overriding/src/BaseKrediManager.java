92646c0e7b77804d12abd3b135f07ed822ba18be
Hasan Gümüş <148052536+hasangumus0707@users.noreply.github.com>
2024-11-30 11:55:52 +0300

    .

---
+++ Java/multiDimensionalArray/Overriding/src/BaseKrediManager.java
@@ -0,0 +1,7 @@
+public class BaseKrediManager {
+
+    // public önüne final yazarsan hesapla fonksiyonunu ezemessin
+    public double hesapla(double tutar) {
+        return tutar * 1.18;
+    }
+}
